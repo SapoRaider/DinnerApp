16b9abc73e7391f6bcd36989d9389ae886cc3ea7
SapoRaider <alejandro.andre.613@gmail.com>
2022-11-18 22:06:42 -0300

    first commit

---
+++ DinnerSV/DinnerApp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'DinnerApp/index.html')
+
+
+def profile(request):
+    return render(request, 'DinnerApp/profile.html')
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data[username]
+            messages.success(request, f'Usuario {username} creado')
+    else:
+        form = UserCreationForm()
+
+    context = {'form' : form}
+
+
+    return render(request, "registration/register.html", context)
